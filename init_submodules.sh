dcb9f4144be8d746becbd7873c3dd8fc1c2a52cf
Marko Kosunen <marko.kosunen@aalto.fi>
2019-06-25 11:57:48 -0700

    Update Scala version to 2.12.3 and remove SNAPSHOT

--- init_submodules.sh
+++ init_submodules.sh
@@ -5,7 +5,7 @@ git submodule update --init
 git submodule sync
 
 #Publish local the ones you need
-for module in clkdiv_n_2_4_8 halfband cic3; do
+for module in halfband cic3; do
     cd $DIR/$module
     sbt publishLocal
 done
